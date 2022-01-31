1faf5d33770fa10c662a734c29a162a6320b02dd
rahul5599 <rahul.mashere@xecurify.com>
2022-01-31 10:24:28 +0530

    done

--- custom.js
+++ custom.js
@@ -22,6 +22,8 @@ $(function() {
     $("#group_multi_select").css({
         "height": "4.3rem !important"
     });
+    $(".btn-group-sm").hide();
+    $(".check-mark").hide();
     let fonts = ``;
     let links = `
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
