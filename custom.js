7c5543c90cd9060a66af184703f49d17f0f07647
rahul5599 <rahul.mashere@xecurify.com>
2022-01-31 10:39:33 +0530

    done

--- custom.js
+++ custom.js
@@ -23,7 +23,7 @@ $(function() {
         "height": "4.3rem !important"
     });
     $(".btn-group-sm").hide();
-    $(".glyphicon-ok").hide();
+    $(".glyphicon.glyphicon-ok").hide();
     let fonts = ``;
     let links = `
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
